16442aa0e0061572d68d4806c6d9f59daa177db5
ya.zmeya.2001@gmail.com <ya.zmeya.2001@gmail.com>
2019-10-08 21:23:47 +0300

    Lab

---
+++ Задание  1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Задание__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Присвоение");
+            string str = Console.ReadLine();
+            double x = Convert.ToDouble(str);
+            int d = (int)(x - Math.Round(x)) * 10;
+            Console.WriteLine("d=" + d);          
+        }
+    }
+}
